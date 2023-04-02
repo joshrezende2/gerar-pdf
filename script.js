0566b053c37f0ee6e8668e65108c7ced4be5bdc2
joshrezende2 <82289034+joshrezende2@users.noreply.github.com>
2023-04-01 23:35:50 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,12 @@
-const os_id = prompt("Please enter the OS ID:");
+var os_id;
 
-fetch(`https://xdzf-voac-tbv6.b2.xano.io/api:QFLVgUNS/os_1?os_id=${os_id}`)
+const referrerUrl = document.referrer;
+const urlSearchParams = new URLSearchParams(referrerUrl.split("?")[1]);
+os_id = urlSearchParams.get("id");
+  fetch(`https://xdzf-voac-tbv6.b2.xano.io/api:QFLVgUNS/os_1?os_id=${os_id}`)
   .then(response => response.json())
   .then(data => {
     const resultElement = document.getElementById("result");
     resultElement.innerText = JSON.stringify(data, null, 2);
-    
-    const generatePdfButton = document.getElementById("generate-pdf");
-    generatePdfButton.addEventListener("click", () => {
-      const pdf = new jsPDF();
-      pdf.text(JSON.stringify(data, null, 2), 10, 10);
-      pdf.save("os_details.pdf");
-    });
+
   });
