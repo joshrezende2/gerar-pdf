2e644d620321788ae7c34baadcb10dac6c33b792
joshrezende2 <82289034+joshrezende2@users.noreply.github.com>
2023-04-01 23:42:01 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,1 @@
-var os_id;
 
-const referrerUrl = document.referrer;
-const urlSearchParams = new URLSearchParams(referrerUrl.split("?")[1]);
-os_id = urlSearchParams.get("id");
-  fetch(`https://xdzf-voac-tbv6.b2.xano.io/api:QFLVgUNS/os_1?os_id=${os_id}`)
-  .then(response => response.json())
-  .then(data => {
-    const resultElement = document.getElementById("result");
-    resultElement.innerText = JSON.stringify(data, null, 2);
-
-  });
