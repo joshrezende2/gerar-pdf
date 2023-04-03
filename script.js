876ba6eb124da25b1d29733841931a36d6477962
joshrezende2 <82289034+joshrezende2@users.noreply.github.com>
2023-04-03 08:48:46 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 
+var os_id;
+
+const referrerUrl = document.referrer;
+const urlSearchParams = new URLSearchParams(referrerUrl.split("?")[1]);
+os_id = urlSearchParams.get("id");
+resultElement.innerText = referrerUrl;
+
