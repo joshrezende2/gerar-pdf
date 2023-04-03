fa53d76f3e6481f75f44435938823df7637ea99c
joshrezende2 <82289034+joshrezende2@users.noreply.github.com>
2023-04-03 09:04:54 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
-
 var os_id;
-
 const referrerUrl = document.referrer;
 const urlSearchParams = new URLSearchParams(referrerUrl.split("?")[1]);
 os_id = urlSearchParams.get("id");
-resultElement.innerText = referrerUrl;
-
+const resultElement = document.getElementById("result");
+if (resultElement) {
+  resultElement.innerText = referrerUrl;
+} else {
+  console.log("The element with ID 'result' doesn't exist.");
+}
